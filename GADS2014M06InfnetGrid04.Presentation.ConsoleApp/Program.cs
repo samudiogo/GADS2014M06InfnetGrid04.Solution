f380558c97e99c212cd357da9aa540834af4ba5a
Supremo Samuel Diogo <samudiogo@gmail.com>
2015-11-05 10:22:09 -0200

    criacao do projeto console app

---
+++ GADS2014M06InfnetGrid04.Presentation.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GADS2014M06InfnetGrid04.Presentation.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
