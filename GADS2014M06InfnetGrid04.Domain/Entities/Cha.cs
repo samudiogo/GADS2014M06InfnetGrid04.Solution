5c39d416b88b9e6c4addc2ec73e136306d753e98
Supremo Samuel Diogo <samudiogo@gmail.com>
2015-11-05 12:22:42 -0200

    implementando a aula 05

--- GADS2014M06InfnetGrid04.Domain/Entities/Cha.cs
+++ GADS2014M06InfnetGrid04.Domain/Entities/Cha.cs
@@ -2,9 +2,33 @@
 
 namespace GADS2014M06InfnetGrid04.Domain.Entities
 {
-    class Cha
+    public class Cha
     {
-        public Guid ChaId { get; set; }
+        #region construtores
+        /// <summary>
+        /// construtor padrão sem argumentos
+        /// Inicializa toda as propriedades com valor padrão
+        /// </summary>
+        public Cha() { }
+        /// <summary>
+        /// construtor com argumentos
+        /// Inicializa todas as propriedades com o sabor passado por argumento 
+        /// </summary>
+        /// <param name="sabor"></param>
+        public Cha(string sabor) // 
+        {
+            Sabor = sabor;
+        }
+        #endregion
+
+        #region propriedades
+        public Guid ChaId { get; set; } = Guid.NewGuid(); // alternativa em C# 6 para inicializar uma propriedade
         public string Sabor { get; set; }
+        #endregion
+
+        public override string ToString()
+        {
+            return $"Chá Id: {ChaId}\nSabor:{Sabor}"; //usando string interpolation
+        }
     }
 }
--- GADS2014M06InfnetGrid04.Presentation.ConsoleApp/Program.cs
+++ GADS2014M06InfnetGrid04.Presentation.ConsoleApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GADS2014M06InfnetGrid04.Domain.Entities;
 
 namespace GADS2014M06InfnetGrid04.Presentation.ConsoleApp
 {
@@ -10,6 +11,54 @@ namespace GADS2014M06InfnetGrid04.Presentation.ConsoleApp
     {
         static void Main(string[] args)
         {
+            // Poderia ser Cafe: Tipo "identificador do objeto"
+            // new Construtor();
+            var cafe = new Cafe();
+
+            cafe.CafeId = Guid.NewGuid();
+            cafe.Forca = 3;
+            cafe.PaisOrigem = "Brasil";
+
+            Console.WriteLine(cafe.CafeId);
+            Console.WriteLine(cafe.Forca);
+            Console.WriteLine(cafe.PaisOrigem);
+
+            QuebraLinha();
+
+            Bebida bebida = new Bebida();
+            bebida.BebidaId = Guid.NewGuid();
+            bebida.DataDeValidade = DateTime.Now;
+            bebida.Nome = "Café com Leite";
+            bebida.Temperatura = 3;
+
+            int temp = bebida.ObterTemperaturaParaServir();
+
+            Console.WriteLine(temp);
+            Console.WriteLine(bebida.Nome);
+
+            QuebraLinha();
+
+            Cha cha = new Cha();
+            Console.WriteLine(cha.ChaId);
+            Console.WriteLine(cha.Sabor);
+
+            QuebraLinha();
+
+            Console.WriteLine("Café Id: {0} => Forca: {1}: Pais: {2}", cafe.CafeId, cafe.Forca, cafe.PaisOrigem);
+
+            QuebraLinha();
+            Console.Write("Entre com o sabor do Chá: ");
+            string texto = Console.ReadLine();
+            var cha3 = new Cha(texto);
+
+            Console.WriteLine(cha3);
+
+
+        }
+
+        public static void QuebraLinha()
+        {
+            Console.WriteLine("\n==================\n");
         }
     }
 }
