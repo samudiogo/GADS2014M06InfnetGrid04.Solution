07c3aabbfacfe96d48592c3f6d49a37b3bc5c84e
Supremo Samuel Diogo <samudiogo@gmail.com>
2015-12-02 21:07:52 -0200

    Criação do projeto Aula 07 e entidade Aluno

---
+++ GADS2014M06InfnetGrid04.Aula07.Domain/Entidades/Aluno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GADS2014M06InfnetGrid04.Aula07.Domain.Entidades
+{
+    public class Aluno
+    {
+        public int AlunoId { get; set; }
+        public string Nome { get; set; }
+
+        public void EstudarMuito()
+        {
+            Console.WriteLine("Estou estudando 10 horas por fim de semana.");
+        }
+
+        public void FazerProvaSemColar()
+        {
+            Console.WriteLine("Fazer a prova com atenção para não cair na pegadinha completo/correto.");
+        }
+    }
+}
