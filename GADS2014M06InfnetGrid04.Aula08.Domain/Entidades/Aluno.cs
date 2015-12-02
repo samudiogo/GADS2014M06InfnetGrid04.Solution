3e4ad3c0b13e83c74365ba791ecaf1fc2dc7cd40
Supremo Samuel Diogo <samudiogo@gmail.com>
2015-12-02 21:19:49 -0200

    Criação do projeto de demonstração da aula 08 com a entidade aluno e sua respectiva interface

---
+++ GADS2014M06InfnetGrid04.Aula08.Domain/Entidades/Aluno.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using GADS2014M06InfnetGrid04.Aula08.Domain.Interfaces;
+
+namespace GADS2014M06InfnetGrid04.Aula08.Domain.Entidades
+{
+    public class Aluno : IAluno
+    {
+        #region Propriedades
+        public Guid AlunoId { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public DateTime DataDeNascimento { get; set; }
+        #endregion
+
+        #region Métodos públicos
+        public bool TemIdadeMinimaParaMatricula()
+        {
+            int idadeMinima = 18;
+            int anoAtual = DateTime.Now.Year;
+            int anoNascimento = DataDeNascimento.Year;
+
+            /* Primeira forma de fazer o "if".
+            if ((anoAtual - anoNascimento) < idadeMinima)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+            */
+
+            // Segunda forma de escrever o mesmo "if".
+            // return ((anoAtual - anoNascimento) < idadeMinima) ? false : true;
+
+            // Terceira e melhor forma de escrever este "if".
+            // O uso do operado ! (not) é necessário para inverter o valor de retorno, 
+            // já que o método espera um retorno true, preferencialemente.
+            return !((anoAtual - anoNascimento) < idadeMinima);
+        }
+        public void FazAlgumaCoisaExclusivoDaClasseAluno()
+        {
+            // código da lógica do método
+        }
+        #endregion
+    }
+}
---
+++ GADS2014M06InfnetGrid04.Aula08.Domain/Interfaces/IAluno.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace GADS2014M06InfnetGrid04.Aula08.Domain.Interfaces
+{
+    public interface IAluno
+    {
+        DateTime DataDeNascimento { get; set; }
+        bool TemIdadeMinimaParaMatricula();
+    }
+}
